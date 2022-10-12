9e390abf037cd695e3d228038d0fa80a4252d712
Matt <mohammadreza.rasolroveicy@polymtl.ca>
2022-10-12 06:05:20 -0700

    makeid query

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,8 +1,13 @@
 const Vehicle = require("../../models/Vehicle");
-
 const Query = {
   vehicles: () => {
     return Vehicle.find().populate("vehicleTypes").exec();
+  },
+  vehicle: (_, args) => {
+    console.log(args.makeId);
+    return Vehicle.findOne({ makeId: args.makeId })
+      .populate("vehicleTypes")
+      .exec();
   },
 };
 
